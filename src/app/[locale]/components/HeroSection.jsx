1af99ffd2ff07ee3fa60378aaae5332f51ddb3a1
Khushi Gupta <khushi@example.com>
2025-08-05 17:17:02 +0530

    add the changes

--- src/app/[locale]/components/HeroSection.jsx
+++ src/app/[locale]/components/HeroSection.jsx
@@ -2,9 +2,22 @@
 
 import { FiArrowUpRight } from "react-icons/fi";
 import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 export default function HeroSection() {
   const t = useTranslations("Hero");
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // handle first load
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // call initially
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <section
@@ -15,21 +28,21 @@ export default function HeroSection() {
 
       <div className="relative z-10 max-w-7xl px-2 top-10">
         <div className="flex flex-col">
-          <h1 className="text-[1.5rem] md:text-[2.5rem] lg:text-[5rem] font-semibold tracking-wider leading-none">
+          <h1 className="text-[1.2rem] md:text-[5rem] lg:text-[4.8rem] font-semibold tracking-wider leading-none">
             {t("heading1")}
           </h1>
-          <h2 className="text-[3rem] md:text-[5rem] lg:text-[10rem] font-extrabold leading-none">
+          <h2 className="text-[5rem] md:text-[4rem] lg:text-[10rem] font-extrabold leading-none">
             {t("heading2")}
           </h2>
         </div>
 
-        <p className="text-[1.1rem] md:text-[1.5rem] lg:text-[1.75rem] text-[#C8C8C8] mb-8">
+        <p className="text-[0.9rem] md:text-[1.5rem] lg:text-[1.75rem] text-[#C8C8C8] mb-8">
           {t("subheading")}
         </p>
 
         <div className="flex justify-center">
           <button className="flex items-center justify-center bg-lime-400 text-[#1D4E00] px-6 py-3 rounded-md font-semibold text-base md:text-lg hover:bg-lime-500 transition">
-            {t("cta")}
+            {isMobile ? t("joinToday") :  t("cta")}
             <FiArrowUpRight className="ml-4 bg-white border border-black rounded-md p-1 w-7 h-7" />
           </button>
         </div>
