3d81c19fe472a684cbfd73c03e2d859e1db77f47
Khushi Gupta <khushi@example.com>
2025-08-09 05:15:11 +0530

    added the changes in the prex

--- src/app/[locale]/components/HowItWorks.jsx
+++ src/app/[locale]/components/HowItWorks.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import process from "../../../../public/assests/howitworks/process.svg";
 import Image from "next/image";
+import Image2 from "../../../../public/Image2.png";
 import { FiArrowUpRight } from "react-icons/fi";
 
 export default function HowItWorks() {
   return (
     <div>
-      <section className="relative bg-[#121212] text-white px-2 md:px-12 w-full pb-8 md:py-16 font-clash">
+      <section className="relative bg-[#121212] text-white px-2 md:px-12 w-full  md:py-16 font-clash">
         {/* Section Heading */}
         <div className="max-w-5xl mx-auto text-left font-bold md:mb-6">
           <h2 className="text-6xl sm:text-4xl md:text-7xl font-bold uppercase">
@@ -16,7 +17,7 @@ export default function HowItWorks() {
 
         {/* Image + Positioned Text */}
         <div className="relative max-w-5xl mx-auto">
-          <div className="w-full h-[300px] relative">
+          <div className="w-full h-[250px] relative">
             {/* SVG Line */}
             <Image
               src={process}
@@ -82,12 +83,22 @@ export default function HowItWorks() {
         </div>
 
         {/* Get Started Button */}
-        <div className="mt-12 flex justify-end pr-4 md:pr-24">
+        <div className="mt-12 flex justify-end pr-4 pb-2 md:pr-24 font-satoshi">
           <button className="bg-[#C6FF00] text-[#1D4E00] px-6 py-3 font-semibold rounded-xl hover:bg-lime-300 transition flex items-center gap-2 text-sm sm:text-base">
-            Get Started
-            <FiArrowUpRight className="ml-4 bg-white rounded-md p-1 w-7 h-7" />
+            GET STARTED
+            <span className="p-1 rounded-md flex items-center justify-center">
+              <Image
+                src={Image2}
+                alt="icon"
+                className="w-4 h-4 sm:w-5 sm:h-5"
+              />
+            </span>
           </button>
         </div>
+
+ 
+        
+        
       </section>
 
       <hr className="hidden sm:block h-0.5 bg-[#ffffff5f] max-w-5xl mx-auto relative z-10" />
