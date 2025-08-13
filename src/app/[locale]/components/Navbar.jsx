7d23ad31e245ea67661c5b43cfc0d95ace02ad1c
Khushi Gupta <khushi@example.com>
2025-08-13 19:30:22 +0530

    add the validation in the

--- src/app/[locale]/components/Navbar.jsx
+++ src/app/[locale]/components/Navbar.jsx
@@ -69,7 +69,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="md:px-0 mx-auto fixed top-6 lg:top-6  left-0 right-0 z-50 s lg:px-4 px-2">
-        <div className="lg:rounded-[50px] rounded-2xl bg-[#0e0c0c15] backdrop-blur text-white border border-[#ffffff33] shadow-[0_0_0.5px_1px_rgba(255,255,255,0.15)]">
+        <div className="lg:rounded-[50px] rounded-2xl  backdrop-blur text-white border border-[#ffffff33] shadow-[0_0_0.5px_1px_rgba(255,255,255,0.15)]">
           {/* Desktop Navigation */}
           <div className="hidden md:flex justify-between items-center px-12 py-4  lg:px-[15px] lg:py-[12px]">
             <div className="flex items-center py-1 space-x-2">
@@ -183,18 +183,18 @@ export default function Navbar() {
             </div>
           </div>
 
-<div className="md:hidden flex items-center justify-between w-full max-w-[399px] h-[56px] px-[14px] py-[7px] mx-auto">
+<div className="md:hidden flex items-center justify-between px-3 py-3 mx-auto">
   {/* Logo */}
   <div className="flex items-center h-full">
     <Image
       src={Logo}
       alt="logo"
-      className="h-full w-auto object-contain"
+      className="h-[26px] w-auto object-contain" // Slightly larger logo
     />
   </div>
 
   {/* Menu Icon */}
-  <button onClick={toggleMobileMenu} className="h-full aspect-square">
+  <button onClick={toggleMobileMenu} className="h-[24px] w-[24px]">
     <Image
       src={menu}
       alt="Mobile Menu Icon"
@@ -202,6 +202,8 @@ export default function Navbar() {
     />
   </button>
 </div>
+
+
 
         </div>
       </nav>
